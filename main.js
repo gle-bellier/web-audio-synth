1fcf7f9594d7055f25ef60392a8aeb301f13f688
Florian Angulo <florian.angulo@gmail.com>
2021-01-23 11:08:46 +0100

    reduced keyboard range

--- main.js
+++ main.js
@@ -67,17 +67,13 @@ function createNoteTable() {
   for (let i=0; i<4; i++) {
     noteFreq[i] = [];
   }
-  for (let i=1; i<3; i++) {
-    if (i!=0){
-      noteFreq[i]["C"] = noteFreq[i-1]["C"] * 2;
-    } else {
-      noteFreq[0]["C"] = 32.703195662574829;
-    }
-    
+
+  noteFreq[0]["C"] = 32.703195662574829*4
+  for (let i=0; i<3; i++) {    
     for (let k=1; k<12; k++) {
       noteFreq[i][listChroma[k]] = noteFreq[i][listChroma[k-1]] * Math.pow(2,1/12);
     }
-    
+    noteFreq[i+1]["C"] = noteFreq[i]["C"]*2
   }
   
   return noteFreq;
